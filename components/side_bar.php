b4765fb8e491082463647c84ae8f8e7a68327fa9
muhammednafih09 <muhammednafih09@gmail.com>
2022-02-17 12:29:22 +0530

    dashboard added

--- components/side_bar.php
+++ components/side_bar.php
@@ -1,50 +1,62 @@
-<div class="d-flex flex-column flex-shrink-0 p-3 pt-5 secondary-color" style="width: 280px; height: 100%;">
+<div class="d-flex flex-column flex-shrink-0 p-3 pt-5 secondary-color side-bar">
     <nav class="navbar fixed-top navbar-light primary-color p-2">
         <a class="navbar-brand text-white" href="#">Icampus</a>
 
-        <input type="search" class="form-control" name="search" id="search" style="width: 250px;" autocomplete="off">
+        <input type="search" class="search-bar" name="search" id="search" style="width: 250px;" autocomplete="off">
     </nav>
     <!-- <hr> -->
     <ul class="nav nav-pills flex-column mb-auto mt-4">
         <li class="nav-item">
             <a href="#" class="nav-link active" aria-current="page">
+                <span class="dashboard"><img src="" alt="" srcset=""></span>
+                Dashboard
+            </a>
+        </li>
+        <hr>
+        <li class="nav-item">
+            <a href="#" class="nav-link link-dark">
                 <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#home"></use>
                 </svg>
                 Profile
             </a>
         </li>
+        <hr>
         <li>
             <a href="#" class="nav-link link-dark">
                 <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#speedometer2"></use>
                 </svg>
-                Add Staff
+                Staff
             </a>
         </li>
+        <hr>
         <li>
             <a href="#" class="nav-link link-dark">
                 <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#table"></use>
                 </svg>
-                Staff Details
+                Student
             </a>
         </li>
+        <hr>
         <li>
             <a href="#" class="nav-link link-dark">
                 <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#grid"></use>
                 </svg>
-                Add Student
+                Settings
             </a>
         </li>
+        <hr>
         <li>
             <a href="#" class="nav-link link-dark">
                 <svg class="bi me-2" width="16" height="16">
                     <use xlink:href="#people-circle"></use>
                 </svg>
-                Student Details
+                Account
             </a>
         </li>
+        <hr>
     </ul>
 </div>
---
+++ dashboard/index.php
@@ -0,0 +1,79 @@
+<?php
+$root_path = '../';
+
+?>
+
+<html lang="en">
+
+<head>
+    <?php require "$root_path/components/head.php"; ?>
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <?php
+    require "$root_path/components/side_bar.php";
+    ?>
+    <div class="container">
+        <div class="" style="margin-left:12%;">
+            <div class="" style="padding-top: 6%;">
+                <div class="row">
+                    <div class="col-12 text-white">
+                        <div class="card primary-color p-2">
+                        <p>Welcome HR</p>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="row">
+                    <div class="col">
+                        <div class="card primary-color text-white" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn secondary-color">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card primary-color text-white" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn secondary-color">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card primary-color text-white" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn secondary-color">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col">
+                        <div class="card primary-color text-white" style="width: 18rem;">
+                            <img class="card-img-top" src="..." alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn secondary-color">Go somewhere</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
